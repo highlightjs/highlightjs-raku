81100c5c820ce2ea27d2411c3821de708d2f8fc0
finanalyst <rnhainsworth@gmail.com>
2020-09-01 12:44:29 +0100

    basic sublang calls in raku

--- src/languages/raku.js
+++ src/languages/raku.js
@@ -8,7 +8,52 @@ export default function(hljs) {
   var RAKU_KEYWORDS = {
     $pattern: /[\w.]+/,
     keyword: ;
+  }
+  var PERL_DEFAULT_CONTAINS = [
+    VAR,
+    { // regexp container
+      begin: '(\\/\\/|' + hljs.RE_STARTERS_RE + '|\\b(split|return|print|reverse|grep)\\b)\\s*',
+      keywords: 'split return print reverse grep',
+      relevance: 0,
+      contains: [
+        {
+          className: 'rakuregex',
+//          begin: '(s|tr|y)/(\\\\.|[^/])*/(\\\\.|[^/])*/[a-z]*',
+//          relevance: 10
+        },
+        {
+          className: 'rakuregex',
+//          begin: '(m|qr)?/', end: '/[a-z]*',
+//          contains: [hljs.BACKSLASH_ESCAPE],
+//          relevance: 0 // allows empty "//" which is a common comment delimiter in other languages
+        }
+      ]
+    },
+    {
+      begin: "^=begin pod",
+      end: "^=end pod$",
+      subLanguage: 'rakudoc',
+      contains: [
+        {
+//            begin: "^@@.*",
+//            end: "$",
+//            className: "comment"
+        }
+      ]
     }
+    {
+//      begin: "^__DATA__$",
+//      end: "^__END__$",
+      subLanguage: 'rakuquoting',
+      contains: [
+        {
+//            begin: "^@@.*",
+//            end: "$",
+//            className: "comment"
+        }
+      ]
+    }
+  ],
 
   SUBST.contains = RAKU_DEFAULT_CONTAINS;
   METHOD.contains = RAKU_DEFAULT_CONTAINS;
